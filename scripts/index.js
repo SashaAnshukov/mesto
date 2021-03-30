376b129f55b287549f8b06ff16b324c51060e50c
Sasha Anshukov <buenosdias2@yandex.ru>
2021-03-30 20:43:13 +0300

    правки после второго ревью

--- scripts/index.js
+++ scripts/index.js
@@ -1,28 +1,14 @@
-console.log('вороны москвички');
-
 let popup = document.querySelector('.popup');
-console.log(popup);
 let openPopupButton = document.querySelector('.profile__edit-button');
-console.log(openPopupButton);
 let closePopupButton = document.querySelector('.popup__close-button');
-console.log(closePopupButton);
 let popupOverlay = document.querySelector('.popup__overlay');
-console.log(popupOverlay);
-//let closeSubmitButton = document.querySelector('.popup__button');
-//console.log(closeSubmitButton);
 let popupContainer = popup.querySelector('.popup__container');
-console.log(popupContainer);
 let profileTitle = document.querySelector('.profile__title');
-console.log(profileTitle.textContent);
 let profileSubTitle = document.querySelector('.profile__subtitle');
-console.log(profileSubTitle.textContent);
-//let popupInput = popup.querySelector('.popup__information');
-//console.log(popupInput);
 let popupTitle = document.querySelector('.popup__input_text_name');
 let popupSubTitle = document.querySelector('.popup__input_text_profession');
 // выбираю класс с наведением
 let noTransition = document.querySelector('.popup__close-button, .opacity-buttons');
-console.log('noTransition');
 
 // Получите значение полей jobInput и nameInput из свойства value
 // в т.ч. добавляю класс наведения при каждом открытии попапа
@@ -49,30 +35,9 @@ popup.classList.remove('popup_visible');
 }
     
 openPopupButton.addEventListener('click', openPopup);
-console.log('OpenClick!');
 closePopupButton.addEventListener('click', closePopup);
-console.log('CloseClick!');
 popupOverlay.addEventListener('click', closePopup);
-console.log('CloseClick!');
+
 // Прикрепляем обработчик к форме:
 // он будет следить за событием “submit” - «отправка»
 popupContainer.addEventListener('submit', formSubmitHandler); 
-//closeSubmitButton.addEventListener('click', closePopup);
-//console.log('CloseClick!');
-//openPopupButton.addEventListener('click', addPopupInfo);
-
-
-/*function addPopupInfo() {
-    popupTitle.value = profileTitle.textContent;
-    popupSubTitle.value = profileSubTitle.textContent;
-}*/
-
-/*function addPopupInfo() {
-    PopupInput.innerHTML = `
-    <div class="popup__close-button"></div>
-    <h3 class="popup__name">Редактировать профиль</h3>
-    <p class="popup__title">${ProfileTitle.textContent}</p>
-    <p class="popup__subtitle">${ProfileSubTitle.textContent}</p>
-    <button class="popup__button">Сохранить</button>
-    `;
-}*/
