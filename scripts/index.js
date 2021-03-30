7bd90a87bfe33ecd8a2784320ec90b4a22f4d281
Sasha Anshukov <buenosdias2@yandex.ru>
2021-03-30 10:51:03 +0300

    правки после првого ревью

--- scripts/index.js
+++ scripts/index.js
@@ -1,72 +1,71 @@
-//.log('вороны москвички');
+console.log('вороны москвички');
 
 let popup = document.querySelector('.popup');
-//console.log(popup);
-
+console.log(popup);
 let openPopupButton = document.querySelector('.profile__edit-button');
-//console.log(openPopupButton);
+console.log(openPopupButton);
 let closePopupButton = document.querySelector('.popup__close-button');
-//console.log(closePopupButton);
-let PopupOverlay = document.querySelector('.popup__overlay');
-//console.log(PopupOverlay);
-let closeSubmitButton = document.querySelector('.popup__button');
+console.log(closePopupButton);
+let popupOverlay = document.querySelector('.popup__overlay');
+console.log(popupOverlay);
+//let closeSubmitButton = document.querySelector('.popup__button');
 //console.log(closeSubmitButton);
+let popupContainer = popup.querySelector('.popup__container');
+console.log(popupContainer);
+let profileTitle = document.querySelector('.profile__title');
+console.log(profileTitle.textContent);
+let profileSubTitle = document.querySelector('.profile__subtitle');
+console.log(profileSubTitle.textContent);
+//let popupInput = popup.querySelector('.popup__information');
+//console.log(popupInput);
+let popupTitle = document.querySelector('.popup__input_text_name');
+let popupSubTitle = document.querySelector('.popup__input_text_profession');
+// выбираю класс с наведением
+let noTransition = document.querySelector('.popup__close-button, .opacity-buttons');
+console.log('noTransition');
 
+// Получите значение полей jobInput и nameInput из свойства value
+// в т.ч. добавляю класс наведения при каждом открытии попапа
 function openPopup() {
     popup.classList.add('popup_visible');
+    noTransition.classList.add('opacity-buttons');
+    popupTitle.value = profileTitle.textContent;
+    popupSubTitle.value = profileSubTitle.textContent;
 }
+// в т.ч. удаляю класс наведения при каждом закрытии попапа для исключения задержки отображения кнопки
 function closePopup() {
     popup.classList.remove('popup_visible');
+    noTransition.classList.remove('opacity-buttons');
 }
 
-openPopupButton.addEventListener('click', openPopup);
-//console.log('OpenClick!');
-closePopupButton.addEventListener('click', closePopup);
-//console.log('CloseClick!');
-PopupOverlay.addEventListener('click', closePopup);
-//console.log('CloseClick!');
-closeSubmitButton.addEventListener('click', closePopup);
-//console.log('CloseClick!');
-
-
-let PopupContainer = popup.querySelector('.popup__container');
-//console.log(PopupContainer);
-
-let ProfileTitle = document.querySelector('.profile__title');
-//console.log(ProfileTitle.textContent);
-let ProfileSubTitle = document.querySelector('.profile__subtitle');
-//console.log(ProfileSubTitle.textContent);
-
-let PopupInput = popup.querySelector('.popup__input');
-//console.log(PopupInput);
-
-function addPopupInfo() {
-    PopupTitle.value = ProfileTitle.textContent;
-    PopupSubTitle.value = ProfileSubTitle.textContent;
-}
-
-openPopupButton.addEventListener('click', addPopupInfo);
-
-    // Получите значение полей jobInput и nameInput из свойства value
-    let  PopupTitle = document.querySelector('.popup__title');
-    let  PopupSubTitle = document.querySelector('.popup__subtitle');
-    
 // Обработчик «отправки» формы,
-    function formSubmitHandler (evt) {
-    
-    evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
-    // Так мы можем определить свою логику отправки.
-    // О том, как это делать, расскажем позже.
-
-    // Вставьте новые значения с помощью textContent
-    ProfileTitle.textContent = PopupTitle.value;
-    ProfileSubTitle.textContent = PopupSubTitle.value;
+function formSubmitHandler (evt) {
+evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
+    // Так мы можем определить свою логику отправки.// О том, как это делать, расскажем позже.
+// Вставьте новые значения с помощью textContent
+profileTitle.textContent = popupTitle.value;
+profileSubTitle.textContent = popupSubTitle.value;
+popup.classList.remove('popup_visible');
 }
     
+openPopupButton.addEventListener('click', openPopup);
+console.log('OpenClick!');
+closePopupButton.addEventListener('click', closePopup);
+console.log('CloseClick!');
+popupOverlay.addEventListener('click', closePopup);
+console.log('CloseClick!');
 // Прикрепляем обработчик к форме:
 // он будет следить за событием “submit” - «отправка»
-PopupContainer.addEventListener('submit', formSubmitHandler); 
+popupContainer.addEventListener('submit', formSubmitHandler); 
+//closeSubmitButton.addEventListener('click', closePopup);
+//console.log('CloseClick!');
+//openPopupButton.addEventListener('click', addPopupInfo);
 
+
+/*function addPopupInfo() {
+    popupTitle.value = profileTitle.textContent;
+    popupSubTitle.value = profileSubTitle.textContent;
+}*/
 
 /*function addPopupInfo() {
     PopupInput.innerHTML = `
