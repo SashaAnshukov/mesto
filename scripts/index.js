ad00f5083f7b080ae4f0817ed5b5ade724b96b20
Sasha Anshukov <buenosdias2@yandex.ru>
2021-04-07 10:52:34 +0300

    добавил popupImage при добавлении новой карточки

--- scripts/index.js
+++ scripts/index.js
@@ -49,7 +49,7 @@ const closeImagePopupButton = openImagePopup.querySelector('.popup__close-button
 const imagePopupImage = openImagePopup.querySelector('.popup__figure-image');
 const imagePopupCaption = openImagePopup.querySelector('.popup__figure-caption');
 
-//
+// Постоянные popup'a и overlay
 const popupOverlay = document.querySelector('.popup__overlay');
 const popupContainer = popup.querySelector('.popup__container');
 const popupContainerImage = popup.querySelector('.popup__container-forImage');
@@ -66,11 +66,27 @@ const noTransitionCloseAddPopupButton = addCardPopup.querySelector('.popup__clos
 
 // Функция формы редактирования профиля
 function formSubmitHandler (evt) {
-evt.preventDefault(); 
-profileTitle.textContent = popupTitle.value;
-profileSubTitle.textContent = popupSubTitle.value;
-popup.classList.remove('popup_visible');
+    evt.preventDefault(); 
+    profileTitle.textContent = popupTitle.value;
+    profileSubTitle.textContent = popupSubTitle.value;
+    popup.classList.remove('popup_visible');
 }
+
+// Функция открытия по клику на картинку
+function fullImagePopup (event) {
+    const allInfoOnClick = event.target.closest('.rectangle');
+    const titleOnClick = allInfoOnClick.querySelector('.rectangle__mesto-text');
+    const popupFullImageCaption =  document.querySelector('.popup__figure-caption');
+    const popupFullImage =  document.querySelector('.popup__figure-image');
+    popupFullImageCaption.textContent = titleOnClick.textContent;
+    popupFullImage.src = event.target.src;
+    togglePopupWindow(openImagePopup);
+};
+
+// Функция like
+function likeButton (evt) {
+    evt.target.classList.toggle('rectangle__mesto-like_active');
+};
 
 // Функция формы добавления новой карточки
 function formSubmitHandlerAddCard (evt) {
@@ -87,10 +103,10 @@ function formSubmitHandlerAddCard (evt) {
     element.prepend(rectangleItem);
     addCardnamePlace.value = "";
     addCardlink.value = "";
+
+    rectangleItemImage.addEventListener ('click', fullImagePopup);
     
-    rectangleItem.querySelector('.rectangle__mesto-like').addEventListener('click', function (evt) {
-        evt.target.classList.toggle('rectangle__mesto-like_active');
-    }); 
+    rectangleItem.querySelector('.rectangle__mesto-like').addEventListener('click', likeButton);
 
     trashButton.addEventListener('click', () => {
         rectangleItem.remove()
@@ -103,7 +119,7 @@ function formSubmitHandlerAddCard (evt) {
 popupContainer.addEventListener('submit', formSubmitHandler); 
 addCardPopup.addEventListener('submit', formSubmitHandlerAddCard);
 
-//Функция карточки Template: Клонирование, like, удаление через кнопку trash, открытие по клику на картинку
+//Функция карточки Template: Клонирование, удаление через кнопку trash
 initialCards.forEach((item) => {
     const rectangleItem = rectangleItemTemplate.cloneNode(true);
     const rectangleItemText =  rectangleItem.querySelector('.rectangle__mesto-text');
@@ -115,25 +131,16 @@ initialCards.forEach((item) => {
     rectangleItemImage.alt = item.name;
     element.prepend(rectangleItem);
     
-    rectangleItem.querySelector('.rectangle__mesto-like').addEventListener('click', function (evt) {
-        evt.target.classList.toggle('rectangle__mesto-like_active');
-    });
+    rectangleItem.querySelector('.rectangle__mesto-like').addEventListener('click', likeButton);
+
     trashButton.addEventListener('click', () => {
         rectangleItem.remove()
     });
     
-    rectangleItemImage.addEventListener ('click', function fullImagePopup (event) {
-        const allInfoOnClick = event.target.closest('.rectangle');
-        const titleOnClick = allInfoOnClick.querySelector('.rectangle__mesto-text');
-        const popupFullImageCaption =  document.querySelector('.popup__figure-caption');
-        const popupFullImage =  document.querySelector('.popup__figure-image');
-        popupFullImageCaption.textContent = titleOnClick.textContent;
-        popupFullImage.src = event.target.src;
-        togglePopupWindow(openImagePopup);
-    });
+    rectangleItemImage.addEventListener ('click', fullImagePopup);
     
     return rectangleItem;
-    });
+});
 
 // Функция открытия/закрытия попапа
 function togglePopupWindow (popup) {
