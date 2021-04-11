fda5c9673b5fe2e129df3a139b6c0d20a46b9e20
Sasha Anshukov <buenosdias2@yandex.ru>
2021-04-11 13:20:41 +0300

    сheckpoint for validation branch

--- scripts/index.js
+++ scripts/index.js
@@ -46,8 +46,8 @@ const closeAddPopupButton = addCardPopup.querySelector('.popup__close-button');
 const closeImagePopupButton = openImagePopup.querySelector('.popup__close-button');
 
 // Постянные popup image
-const imagePopupImage = openImagePopup.querySelector('.popup__figure-image');
-const imagePopupCaption = openImagePopup.querySelector('.popup__figure-caption');
+const popupFullImageCaption =  document.querySelector('.popup__figure-caption');
+const popupFullImage =  document.querySelector('.popup__figure-image');
 
 // Постоянные popup'a и overlay
 const popupOverlay = document.querySelector('.popup__overlay');
@@ -73,54 +73,55 @@ function formSubmitHandler (evt) {
 }
 
 // Функция открытия по клику на картинку
-function fullImagePopup (event) {
+/*function getFullImagePopup (event) {
     const allInfoOnClick = event.target.closest('.rectangle');
     const titleOnClick = allInfoOnClick.querySelector('.rectangle__mesto-text');
-    const popupFullImageCaption =  document.querySelector('.popup__figure-caption');
-    const popupFullImage =  document.querySelector('.popup__figure-image');
     popupFullImageCaption.textContent = titleOnClick.textContent;
     popupFullImage.src = event.target.src;
     togglePopupWindow(openImagePopup);
-};
+};*/
+
+/*function getFullImagePopup (image) {
+    rectangle__image.addEventListener('click', () => {
+    popupFullImageCaption = image.alt;
+    popupFullImage = image.src;
+    togglePopupWindow(openImagePopup);
+    });
+}*/
+
 
 // Функция like
 function likeButton (evt) {
     evt.target.classList.toggle('rectangle__mesto-like_active');
 };
+////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-// Функция формы добавления новой карточки
-function formSubmitHandlerAddCard (evt) {
-    evt.preventDefault(); 
-    const inputAddnamePlace = addCardnamePlace.value;
-    const inputAddLink = addCardlink.value;
+function createNewCard(item) {
     const rectangleItem = rectangleItemTemplate.cloneNode(true);
     const rectangleItemText =  rectangleItem.querySelector('.rectangle__mesto-text');
     const rectangleItemImage =  rectangleItem.querySelector('.rectangle__image');
     const trashButton = rectangleItem.querySelector('.rectangle__trash');
-    rectangleItemText.textContent = inputAddnamePlace;
-    rectangleItemImage.src = inputAddLink;
-    rectangleItemImage.alt = inputAddnamePlace;
-    element.prepend(rectangleItem);
-    addCardnamePlace.value = "";
-    addCardlink.value = "";
-
-    rectangleItemImage.addEventListener ('click', fullImagePopup);
-    
+    rectangleItemText.textContent = item.name;
+    rectangleItemImage.src = item.link;
+    rectangleItemImage.alt = item.name;
     rectangleItem.querySelector('.rectangle__mesto-like').addEventListener('click', likeButton);
-
     trashButton.addEventListener('click', () => {
         rectangleItem.remove()
     });
-
-    addCardPopup.classList.remove('popup_visible');
+    // Функция открытия по клику на картинку
+    rectangleItemImage.addEventListener ('click', () => {
+        popupFullImageCaption.textContent = rectangleItemImage.alt;
+        popupFullImage.src = rectangleItemImage.src;
+        togglePopupWindow(openImagePopup);
+    });
+    return rectangleItem;
 }
 
-//Обработчики форм редактирования профиля и добавления новой карточки
-popupContainer.addEventListener('submit', formSubmitHandler); 
-addCardPopup.addEventListener('submit', formSubmitHandlerAddCard);
 
-//Функция карточки Template: Клонирование, удаление через кнопку trash
-initialCards.forEach((item) => {
+///////////////////////////////////////////////////////////////////////////////////////////////////////
+/*function createNewCard(name, link) {
+    const inputAddnamePlace = addCardnamePlace.value;
+    const inputAddLink = addCardlink.value
     const rectangleItem = rectangleItemTemplate.cloneNode(true);
     const rectangleItemText =  rectangleItem.querySelector('.rectangle__mesto-text');
     const rectangleItemImage =  rectangleItem.querySelector('.rectangle__image');
@@ -130,17 +131,50 @@ initialCards.forEach((item) => {
     rectangleItemImage.src = item.link;
     rectangleItemImage.alt = item.name;
     element.prepend(rectangleItem);
-    
-    rectangleItem.querySelector('.rectangle__mesto-like').addEventListener('click', likeButton);
 
-    trashButton.addEventListener('click', () => {
+    /*rectangleItemText.textContent = inputAddnamePlace;
+    rectangleItemImage.src = inputAddLink;
+    rectangleItemImage.alt = inputAddnamePlace;
+    element.prepend(rectangleItem);*/
+
+    /*rectangleItemImage.addEventListener ('click', getFullImagePopup);
+    rectangleItem.querySelector('.rectangle__mesto-like').addEventListener('click', likeButton);////////
+    trashButton.addEventListener('click', () => {///////////////////////////////////////////////////////
         rectangleItem.remove()
     });
-    
-    rectangleItemImage.addEventListener ('click', fullImagePopup);
-    
+    element.prepend(rectangleItem);
     return rectangleItem;
+}*/
+
+/*const newCard = createNewCard(inputAddnamePlace, inputAddLink);
+element.prepend(newCard);*/
+
+//Добавление клонированных карточек с данными из массива
+initialCards.forEach((item) => {
+    const generateCard = createNewCard(item);
+    element.prepend(generateCard);
 });
+
+// Функция формы добавления новой карточки
+function formSubmitHandlerAddCard (evt) {
+    evt.preventDefault(); 
+    inputAddnamePlace = addCardnamePlace.value;
+    inputAddLink = addCardlink.value;
+    //const cardItems = {name: inputAddnamePlace, link: inputAddLink};
+    const newCard = createNewCard(inputAddnamePlace, inputAddLink);
+    //const newCard = createNewCard(cardItems);
+    
+    addCardnamePlace.value = "";
+    addCardlink.value = "";
+    element.prepend(newCard);
+    togglePopupWindow(addCardPopup);
+}
+
+//Обработчики форм редактирования профиля и добавления новой карточки
+popupContainer.addEventListener('submit', formSubmitHandler); 
+addCardPopup.addEventListener('submit', formSubmitHandlerAddCard);
+
+
 
 // Функция открытия/закрытия попапа
 function togglePopupWindow (popup) {
@@ -150,7 +184,7 @@ function togglePopupWindow (popup) {
 //Открытие/закртытие карточкек редактирования профиля/добавления карточки, с функцией 
 openEditPopupButton.addEventListener('click', () => 
     togglePopupWindow(editPopup),
-    inputAddnamePlace = addCardnamePlace.value,
+    //inputAddnamePlace = addCardnamePlace.value,
     popupTitle.value = profileTitle.textContent,
     popupSubTitle.value = profileSubTitle.textContent,
 );
