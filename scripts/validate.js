d59eb7b3f9db60142064c22dcd81681fb2fb09d5
Sasha Anshukov <buenosdias2@yandex.ru>
2021-04-15 19:42:41 +0300

    сheckpont valiate 15.04.2021

--- scripts/validate.js
+++ scripts/validate.js
@@ -1,25 +1,64 @@
 console.log('вороны москвички');
 
-const checkInputValidity = (inputElement) => {
-    const isInputElementValid = inputElement.validity.valid;
-    console.log(isInputElementValid);
+//функция пояления ошибки. Подсвечивает инпут нужным классом и показывать текст ошибки
+const showInputError = (inputElement, errorMessage) => {
+    console.log(inputElement.name, errorMessage);
+    const errorElement = inputElement.closest('.popup__label').querySelector('.popup__input-error');
+    errorElement.textContent = errorMessage;
+    errorElement.classList.add('popup__input-error_active');
+}
+
+//функция скытия ошибки
+const hideInputError = (inputElement) => {
+    const errorElement = inputElement.closest('.popup__label').querySelector('.popup__input-error');
+    errorElement.textContent = "";
+    errorElement.classList.remove('popup__input-error_active');
+}
+
+//функция проверки валидности инпута по длине и type
+const checkInputValidity = (formElement, inputElement) => {
+    const isInputNotValid = !inputElement.validity.valid;
+    //console.log(isInputNotValid);
+    //если не валиден, то показываем ошибку
+    if (isInputNotValid) {
+        const errorMessage = inputElement.validationMessage;
+        showInputError(inputElement, errorMessage);
+    } 
+    else {
+        hideInputError(inputElement);
+    }
 };
 
-const setEventListeners = (formElement) => {
-    
-    const inputList = Array.from(formElement.querySelectorAll('.popup__input'));
-    console.log(inputList);
+//функция неактивной кнопки при ошибке в инпуте
+const toggleButtonState = (inputList, buttonElement) => {
+    const hasNotValidIput = inputList.some((inputElement) => !inputElement.validity.valid);
+    if (hasNotValidIput) {
+        buttonElement.setAttribute('disabled', true);
+    } else
+    buttonElement.removeAttribute('disabled');
+}
 
+//функция, которая приимает на вход формы и устанавливает обработчики
+const setEventListeners = (formElement) => {
+    //получим список инпутов
+    const inputList = Array.from(formElement.querySelectorAll('.popup__input'));
+    //найдём кнопку
+    const buttonElement = formElement.querySelector('.popup__button');
+    console.log(inputList);
+    //повесим событие ввода на инпут
     inputList.forEach((inputElement) => {
-        inputElement.addEventlistener('input', (event) => {
-            checkInputValidity(inputElement);
+        inputElement.addEventListener ('input', function (event) {
+            //проверяем валидность инпута
+            checkInputValidity(formElement, inputElement);
+            toggleButtonState(inputList, buttonElement);
         });
     });
-    
+    toggleButtonState(inputList, buttonElement);
 };
 
 //функция валидации
 const enableValidation = () => {
+    //находим все формы на сранице
     const formList = Array.from(document.querySelectorAll('.popup__form'));
     
     console.log(formList);
